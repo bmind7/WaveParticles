406e71759dac40236aa93b0ec2dc743b1b6b9002
Viktor Zatorskyi <victor.zatorski@gmail.com>
2020-08-28 21:23:33 +0300

    Add: WaveDebug system and set up it in the scene

---
+++ Assets/Scripts/Libs/FluidSim/WaveParticles/WaveDebugSystem.cs
@@ -0,0 +1,64 @@
+﻿using OneBitLab.Services;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace OneBitLab.FluidSim
+{
+    public struct WaveDebugData
+    {
+        public float2 Pos;
+        public float2 Dir;
+        public float2 Origin;
+        public float  TimeToReflect;
+    }
+
+    [UpdateAfter( typeof(WaveReflectSystem) )]
+    public class WaveDebugSystem : SystemBase
+    {
+        //-------------------------------------------------------------
+        public readonly NativeQueue<WaveDebugData>
+            m_DebugQueue = new NativeQueue<WaveDebugData>( Allocator.Persistent );
+
+        //-------------------------------------------------------------
+        protected override void OnDestroy()
+        {
+            m_DebugQueue.Dispose();
+        }
+
+        //-------------------------------------------------------------
+        protected override void OnUpdate()
+        {
+            if( !Input.GetMouseButton( 1 ) )
+            {
+                // Only do work if RMB is pressed
+                return;
+            }
+
+            Ray    ray        = ResourceLocatorService.Instance.m_MainCam.ScreenPointToRay( Input.mousePosition );
+            float  dist       = math.abs( ray.origin.y / ray.direction.y );
+            float3 hitPoint   = ray.GetPoint( dist );
+            float  radiusSq   = 0.5f * 0.5f;
+            var    debugQueue = m_DebugQueue;
+
+            Entities
+                .ForEach( ( in WavePos wPos, in WaveDir wDir, in WaveOrigin wOrigin, in TimeToReflect ttr ) =>
+                {
+                    if( math.distancesq( wPos.Value, hitPoint.xz ) > radiusSq )
+                    {
+                        return;
+                    }
+
+                    debugQueue.Enqueue( new WaveDebugData
+                    {
+                        Pos = wPos.Value, Dir = wDir.Value, Origin = wOrigin.Value, TimeToReflect = ttr.Value
+                    } );
+                } )
+                .Run();
+        }
+
+        //-------------------------------------------------------------
+    }
+}
---
+++ Assets/Scripts/Libs/FluidSim/WaveParticles/WaveDebugVisualizer.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace OneBitLab.FluidSim
+{
+    public class WaveDebugVisualizer : MonoBehaviour
+    {
+        //-------------------------------------------------------------
+        private NativeQueue<WaveDebugData> m_DebugQueue;
+
+        //-------------------------------------------------------------
+        private void Awake()
+        {
+            m_DebugQueue = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<WaveDebugSystem>().m_DebugQueue;
+        }
+
+        //-------------------------------------------------------------
+        private void OnDrawGizmos()
+        {
+            if( !m_DebugQueue.IsCreated ) return;
+
+            while( m_DebugQueue.TryDequeue( out WaveDebugData data ) )
+            {
+                // Debug.Log( time );
+                var     center       = new Vector3( data.Pos.x, 0, data.Pos.y );
+                var     origin       = new Vector3( data.Origin.x, 0, data.Origin.y );
+                var     dir          = new Vector3( data.Dir.x, 0, data.Dir.y );
+                Vector3 reflectPoint = center + WaveSpawnSystem.c_WaveParticleSpeed * data.TimeToReflect * dir;
+
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere( center, 0.1f );
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine( center, reflectPoint );
+                Gizmos.DrawWireSphere( reflectPoint, 0.02f );
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere( origin, 0.02f );
+                Gizmos.DrawLine( origin, center );
+            }
+        }
+
+        //-------------------------------------------------------------
+    }
+}
